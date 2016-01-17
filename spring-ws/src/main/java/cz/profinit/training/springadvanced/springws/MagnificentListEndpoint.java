e1ef51582d4c04d7dd878a4ee4e427e3956b0314
tomaspinos <tomas.pinos@gmail.com>
2016-01-17 22:56:58 +0100

    spring-ws - Zprovoznění, drobný refactoring.

--- spring-ws/src/main/java/cz/profinit/training/springadvanced/springws/MagnificentListEndpoint.java
+++ spring-ws/src/main/java/cz/profinit/training/springadvanced/springws/MagnificentListEndpoint.java
@@ -2,6 +2,7 @@ package cz.profinit.training.springadvanced.springws;
 
 import cz.profinit.springadvanced.schemas.GetListIdsRequest;
 import cz.profinit.springadvanced.schemas.GetListIdsResponse;
+import cz.profinit.training.springadvanced.domain.MagnificentList;
 import cz.profinit.training.springadvanced.service.MagnificentListService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -16,11 +17,11 @@ public class MagnificentListEndpoint {
 
     private static final String SPRING_ADVANCED_NAMESPACE = "http://profinit.cz/springadvanced/schemas";
 
-    private final MagnificentListService magnificentListService;
+    private final MagnificentListService service;
 
     @Autowired
-    public MagnificentListEndpoint(MagnificentListService magnificentListService) {
-        this.magnificentListService = magnificentListService;
+    public MagnificentListEndpoint(MagnificentListService service) {
+        this.service = service;
     }
 
     @PayloadRoot(namespace = SPRING_ADVANCED_NAMESPACE, localPart = "GetListIdsRequest")
@@ -28,9 +29,11 @@ public class MagnificentListEndpoint {
     @ResponsePayload
     GetListIdsResponse getListIds(@RequestPayload GetListIdsRequest request) {
         GetListIdsResponse getListIdsResponse = new GetListIdsResponse();
-        GetListIdsResponse.ListId listId = new GetListIdsResponse.ListId();
-        listId.setId(BigInteger.valueOf(10L));
-        getListIdsResponse.getListId().add(listId);
+        for (MagnificentList magnificentList : service.getLists()) {
+            GetListIdsResponse.ListId listId = new GetListIdsResponse.ListId();
+            listId.setId(BigInteger.valueOf(magnificentList.getId()));
+            getListIdsResponse.getListId().add(listId);
+        }
         return getListIdsResponse;
     }
 
