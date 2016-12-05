c78c1d9aa2570772f5578ec4f6fe3ea84451420b
tomaspinos <tomas.pinos@gmail.com>
2016-12-05 21:56:48 +0100

    Lab - servlet-api.

--- servlet-api/src/main/java/cz/profinit/training/springadvanced/servletapi/RequestCounterServlet.java
+++ servlet-api/src/main/java/cz/profinit/training/springadvanced/servletapi/RequestCounterServlet.java
@@ -1,8 +1,5 @@
 package cz.profinit.training.springadvanced.servletapi;
 
-import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.context.support.WebApplicationContextUtils;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,14 +10,6 @@ public class RequestCounterServlet extends HttpServlet {
 
     @Override
     protected void doGet(final HttpServletRequest request, final HttpServletResponse response) throws ServletException, IOException {
-        final WebApplicationContext ctx = WebApplicationContextUtils.getRequiredWebApplicationContext(request.getSession().getServletContext());
-
-        final RequestCounterBean sessionScoped = ctx.getBean("sessionRqCounter", RequestCounterBean.class);
-        final RequestCounterBean singletonScoped = ctx.getBean("singletonRqCounter", RequestCounterBean.class);
-        final RequestCounterBean prototypeScoped = ctx.getBean("prototypeRqCounter", RequestCounterBean.class);
-
-        response.getWriter().write("Session counter: " + sessionScoped.inc() + "\n");
-        response.getWriter().write("Singleton counter: " + singletonScoped.inc() + "\n");
-        response.getWriter().write("Prototype counter: " + prototypeScoped.inc() + "\n");
+        // TODO
     }
 }
