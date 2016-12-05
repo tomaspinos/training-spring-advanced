4ca7058539b7cae88052a21265bbd7fc3bd4a2b6
tomaspinos <tomas.pinos@gmail.com>
2016-12-05 19:26:27 +0100

    Priklady - ladeni.

--- spring-rest-chat/service/src/main/java/cz/profinit/training/springadvanced/springrest/chat/controller/ChatController.java
+++ spring-rest-chat/service/src/main/java/cz/profinit/training/springadvanced/springrest/chat/controller/ChatController.java
@@ -1,19 +1,21 @@
 package cz.profinit.training.springadvanced.springrest.chat.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import cz.profinit.training.springadvanced.springrest.chat.lifecycle.ChatLifecycle;
 import cz.profinit.training.springadvanced.springrest.chat.model.ChatRating;
 import cz.profinit.training.springadvanced.springrest.chat.model.ChatRatingResponse;
 import cz.profinit.training.springadvanced.springrest.chat.model.ChatUpdate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/chat")
@@ -26,49 +28,49 @@ public class ChatController {
         this.lifecycle = lifecycle;
     }
 
-    @RequestMapping(value = "/status", method = RequestMethod.GET)
+    @GetMapping("/status")
     @ResponseStatus(HttpStatus.OK)
     public ChatUpdate status() {
         return lifecycle.status();
     }
 
-    @RequestMapping(value = "/conversation", method = RequestMethod.POST)
+    @PostMapping("/conversation")
     @ResponseStatus(HttpStatus.CREATED)
     public ChatUpdate start() {
         return lifecycle.start();
     }
 
-    @RequestMapping(value = "/conversation/{sessionId}/message", method = RequestMethod.POST)
+    @PostMapping("/conversation/{sessionId}/message")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ChatUpdate send(@PathVariable final String sessionId, @RequestParam final String text) {
         return lifecycle.sendMessage(sessionId, text);
     }
 
-    @RequestMapping(value = "/conversation/{sessionId}/message/{messageId}", method = RequestMethod.PUT)
+    @PutMapping("/conversation/{sessionId}/message/{messageId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ChatUpdate modify(@PathVariable final String sessionId, @PathVariable final String messageId, @RequestParam final String text) {
         return lifecycle.modifyMessage(sessionId, messageId, text);
     }
 
-    @RequestMapping(value = "/conversation/{sessionId}/message/{messageId}", method = RequestMethod.DELETE)
+    @DeleteMapping("/conversation/{sessionId}/message/{messageId}")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ChatUpdate delete(@PathVariable final String sessionId, @PathVariable final String messageId) {
         return lifecycle.deleteMessage(sessionId, messageId);
     }
 
-    @RequestMapping(value = "/conversation/{sessionId}", method = RequestMethod.GET)
+    @GetMapping("/conversation/{sessionId}")
     @ResponseStatus(HttpStatus.OK)
     public ChatUpdate refresh(@PathVariable final String sessionId) {
         return lifecycle.refresh(sessionId);
     }
 
-    @RequestMapping(value = "/conversation/{sessionId}", method = RequestMethod.DELETE)
+    @DeleteMapping("/conversation/{sessionId}")
     @ResponseStatus(HttpStatus.OK)
     public ChatUpdate finish(@PathVariable final String sessionId) {
         return lifecycle.finish(sessionId);
     }
 
-    @RequestMapping(value = "/conversation/{sessionId}/rating", method = RequestMethod.POST)
+    @PostMapping("/conversation/{sessionId}/rating")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public ChatRatingResponse rating(@PathVariable final String sessionId, @RequestBody final ChatRating rating) {
         return lifecycle.rating(sessionId, rating);
