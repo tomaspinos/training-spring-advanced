ec3ef3a57ae6bd2b7c27f098a5a818619ef85e66
tomaspinos <tomas.pinos@gmail.com>
2016-12-05 20:51:02 +0100

    Lab - warmup.

--- spring-warmup/src/main/java/cz/profinit/training/springadvanced/warmup/WarmupApplication.java
+++ spring-warmup/src/main/java/cz/profinit/training/springadvanced/warmup/WarmupApplication.java
@@ -1,14 +1,10 @@
 package cz.profinit.training.springadvanced.warmup;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
 public class WarmupApplication {
 
     public static void main(String[] args) {
-        final AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(WarmupConfiguration.class);
-
-        final WarmupService warmupService = applicationContext.getBean(WarmupService.class);
-
-        System.out.println(warmupService.sayHello("world"));
+        // TODO create application context
+        // TODO get WarmupService bean
+        // TODO say hello
     }
 }
