ad32a448c708227dbb059b0d1f84969e87d801b6
tomaspinos <tomas.pinos@gmail.com>
2016-12-06 22:12:12 +0100

    spring-security priklady.

---
+++ spring-rest-chat/service-secured/src/main/java/cz/profinit/training/springadvanced/springrest/chat/lifecycle/ChatSecuredLifecycle.java
@@ -0,0 +1,153 @@
+package cz.profinit.training.springadvanced.springrest.chat.lifecycle;
+
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatMessage;
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatRating;
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatRatingResponse;
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatUpdate;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import static cz.profinit.training.springadvanced.springrest.chat.model.ChatMessageDirectionType.INCOMING;
+import static cz.profinit.training.springadvanced.springrest.chat.model.ChatMessageDirectionType.OUTGOING;
+import static cz.profinit.training.springadvanced.springrest.chat.model.ChatStatusType.AVAILABLE;
+import static cz.profinit.training.springadvanced.springrest.chat.model.ChatStatusType.FINISHED;
+import static cz.profinit.training.springadvanced.springrest.chat.model.ChatStatusType.RUNNING;
+import static java.util.Collections.singletonList;
+
+@Component
+public class ChatSecuredLifecycle {
+
+    private static final String[] WELCOME_MESSAGES = {
+            "Welcome!",
+            "Here we go!",
+            "What can I do you for?",
+            "Identify <yourself>!",
+            "State your purpose!"
+    };
+
+    private static final String[] INCOMING_MESSAGES = {
+            "Ok",
+            "Very well!",
+            "Well done!",
+            "Really?",
+            "I'm sorry to hear that..."
+    };
+
+    private static final String[] GOODBYE_MESSAGES = {
+            "Thanks!",
+            "Enough is enough"
+    };
+
+    private static final Log logger = LogFactory.getLog(ChatSecuredLifecycle.class);
+
+    private final Set<String> runningSessionIds = new HashSet<>();
+    private final Set<String> deletedSessionIds = new HashSet<>();
+
+    private final Random random = new Random();
+
+    public ChatUpdate status() {
+        logger.info("status");
+        return new ChatUpdate(AVAILABLE);
+    }
+
+    public ChatUpdate start() {
+        logger.info("start");
+        return new ChatUpdate(RUNNING, createSession(), singletonList(new ChatMessage(INCOMING, randomWelcomeMessage())));
+    }
+
+    public ChatUpdate sendMessage(final String sessionId, final String text) {
+        logger.info("send");
+        verifySessionIsRunning(sessionId);
+        return new ChatUpdate(RUNNING, sessionId, singletonList(new ChatMessage(OUTGOING, text)), randomMessageId());
+    }
+
+    public ChatUpdate getMessage(final String sessionId, final String messageId) {
+        logger.info("getMessage");
+        verifySessionIsRunning(sessionId);
+        return new ChatUpdate(RUNNING, sessionId, singletonList(new ChatMessage(OUTGOING, "Censored")), messageId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ChatUpdate modifyMessage(final String sessionId, final String messageId, final String text) {
+        logger.info("modify");
+        verifySessionIsRunning(sessionId);
+        return new ChatUpdate(RUNNING, sessionId, singletonList(new ChatMessage(OUTGOING, text)), messageId);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ChatUpdate deleteMessage(final String sessionId, final String messageId) {
+        logger.info("delete");
+        verifySessionIsRunning(sessionId);
+        return new ChatUpdate(RUNNING, sessionId, Collections.emptyList(), messageId);
+    }
+
+    public ChatUpdate refresh(final String sessionId) {
+        logger.info("refresh");
+        verifySessionIsRunning(sessionId);
+        return new ChatUpdate(RUNNING, sessionId, singletonList(new ChatMessage(INCOMING, randomIncomingMessage())));
+    }
+
+    public ChatUpdate finish(final String sessionId) {
+        logger.info("finish");
+        verifySessionIsRunning(sessionId);
+        deleteSession(sessionId);
+        return new ChatUpdate(FINISHED, sessionId, singletonList(new ChatMessage(INCOMING, randomGoodbyeMessage())));
+    }
+
+    public ChatRatingResponse rating(final String sessionId, final ChatRating rating) {
+        logger.info("rating");
+        verifySessionIsRunningOrDeleted(sessionId);
+        return new ChatRatingResponse(sessionId, rating, randomGoodbyeMessage());
+    }
+
+    private String createSession() {
+        final String sessionId = randomSessionId();
+        runningSessionIds.add(sessionId);
+        return sessionId;
+    }
+
+    private void deleteSession(final String sessionId) {
+        runningSessionIds.remove(sessionId);
+        deletedSessionIds.add(sessionId);
+    }
+
+    private void verifySessionIsRunning(final String sessionId) {
+        if (!runningSessionIds.contains(sessionId)) {
+            throw new ChatSessionNotFoundException(sessionId);
+        }
+    }
+
+    private void verifySessionIsRunningOrDeleted(final String sessionId) {
+        if (!runningSessionIds.contains(sessionId) && !deletedSessionIds.contains(sessionId)) {
+            throw new ChatSessionNotFoundException(sessionId);
+        }
+    }
+
+    private String randomSessionId() {
+        return RandomStringUtils.randomAlphanumeric(12).toUpperCase();
+    }
+
+    private String randomMessageId() {
+        return RandomStringUtils.randomAlphanumeric(16).toUpperCase();
+    }
+
+    private String randomWelcomeMessage() {
+        return WELCOME_MESSAGES[random.nextInt(WELCOME_MESSAGES.length)];
+    }
+
+    private String randomIncomingMessage() {
+        return INCOMING_MESSAGES[random.nextInt(INCOMING_MESSAGES.length)];
+    }
+
+    private String randomGoodbyeMessage() {
+        return GOODBYE_MESSAGES[random.nextInt(GOODBYE_MESSAGES.length)];
+    }
+}
---
+++ spring-rest-chat/service-secured/src/test/java/cz/profinit/training/springadvanced/springrest/chat/controller/ChatSecuredControllerTest.java
@@ -0,0 +1,85 @@
+package cz.profinit.training.springadvanced.springrest.chat.controller;
+
+import cz.profinit.training.springadvanced.springrest.chat.ChatServiceSecuredApplication;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.StandardCharsets;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ChatServiceSecuredApplication.class)
+@WebAppConfiguration
+public class ChatSecuredControllerTest {
+
+    private static final MediaType JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(),
+            MediaType.APPLICATION_JSON.getSubtype(), StandardCharsets.UTF_8);
+
+    private static final MediaType XML_UTF8 = new MediaType(MediaType.APPLICATION_XML.getType(),
+            MediaType.APPLICATION_XML.getSubtype(), StandardCharsets.UTF_8);
+
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext applicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(applicationContext).build();
+    }
+
+    @Test
+    public void statusIsOk() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/chat/status")).
+                andExpect(MockMvcResultMatchers.status().isOk()).
+                andExpect(MockMvcResultMatchers.content().contentType(JSON_UTF8)).
+                andExpect(MockMvcResultMatchers.jsonPath("status", Matchers.is("AVAILABLE")));
+    }
+
+    @Test
+    public void statusIsOkXml() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/chat/status").accept(XML_UTF8)).
+                andExpect(MockMvcResultMatchers.status().isOk()).
+                andExpect(MockMvcResultMatchers.content().contentType(XML_UTF8)).
+                andExpect(MockMvcResultMatchers.xpath("//status").string("AVAILABLE"));
+    }
+
+    @Test
+    public void startReturnsSessionIdAndWelcomeMessage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/chat/conversation")).
+                andExpect(MockMvcResultMatchers.status().isCreated()).
+                andExpect(MockMvcResultMatchers.content().contentType(JSON_UTF8)).
+                andExpect(MockMvcResultMatchers.jsonPath("sessionId", Matchers.not(Matchers.isEmptyOrNullString()))).
+                andExpect(MockMvcResultMatchers.jsonPath("status", Matchers.is("RUNNING"))).
+                andExpect(MockMvcResultMatchers.jsonPath("messages", Matchers.hasSize(1))).
+                andExpect(MockMvcResultMatchers.jsonPath("messages[0]", Matchers.not(Matchers.isEmptyOrNullString())));
+    }
+
+    @Test
+    public void startReturnsSessionIdAndWelcomeMessageXml() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/chat/conversation").accept(XML_UTF8)).
+                andExpect(MockMvcResultMatchers.status().isCreated()).
+                andExpect(MockMvcResultMatchers.content().contentType(XML_UTF8)).
+                andExpect(MockMvcResultMatchers.xpath("//sessionId").string(Matchers.notNullValue())).
+                andExpect(MockMvcResultMatchers.xpath("//status").string("RUNNING")).
+                andExpect(MockMvcResultMatchers.xpath("//messages").nodeCount(1)).
+                andExpect(MockMvcResultMatchers.xpath("//messages[position() = 1]").string(Matchers.notNullValue()));
+    }
+
+    @Test
+    public void refreshReturnsNotFoundStatusForNonexistingSessionId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/chat/conversation/XXXERRRORXXX"))
+                .andExpect(MockMvcResultMatchers.status().isNotFound());
+    }
+}
