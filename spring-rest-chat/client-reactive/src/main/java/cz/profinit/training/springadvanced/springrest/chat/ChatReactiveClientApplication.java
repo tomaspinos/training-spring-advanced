bb1e4f8a18b44f3d87a3a5684f1765930b4e322e
Tomáš Piňos <tomas.pinos@gmail.com>
2017-11-13 22:36:28 +0100

    Chat - reactive client.

---
+++ spring-rest-chat/client-reactive/src/main/java/cz/profinit/training/springadvanced/springrest/chat/ChatReactiveClientApplication.java
@@ -0,0 +1,60 @@
+package cz.profinit.training.springadvanced.springrest.chat;
+
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatRating;
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatRatingResponse;
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatUpdate;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class ChatReactiveClientApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+        WebClient webClient = WebClient.create("http://localhost:8080/chat");
+
+        ChatUpdate status = webClient.get()
+                .uri("/status")
+                .exchange()
+                .flatMap(response -> response.bodyToMono(ChatUpdate.class))
+                .block();
+
+        System.out.println("status = " + status);
+
+        ChatUpdate start = webClient.post()
+                .uri("/conversation")
+                .exchange()
+                .flatMap(response -> response.bodyToMono(ChatUpdate.class))
+                .block();
+
+        System.out.println("start = " + start);
+
+        ChatUpdate firstMessage = webClient.post()
+                .uri("/conversation/{sessionId}/message?text=Howdoyoudo", start.getSessionId())
+                .exchange()
+                .flatMap(response -> response.bodyToMono(ChatUpdate.class))
+                .block();
+
+        System.out.println("firstMessage = " + firstMessage);
+
+        for (int i = 0; i < 3; i++) {
+            ChatUpdate update = webClient.get()
+                    .uri("/conversation/{sessionId}", start.getSessionId())
+                    .exchange()
+                    .flatMap(response -> response.bodyToMono(ChatUpdate.class))
+                    .block();
+
+            System.out.println("Refresh: " + update);
+        }
+
+        System.out.println("Finish");
+        webClient.delete().uri("/conversation/{sessionId}", start.getSessionId()).exchange().block();
+
+        ChatRatingResponse rating = webClient.post()
+                .uri("/conversation/{sessionId}/rating", start.getSessionId())
+                .body(BodyInserters.fromObject(new ChatRating(10, "scott", "Rather good")))
+                .exchange()
+                .flatMap(response -> response.bodyToMono(ChatRatingResponse.class))
+                .block();
+
+        System.out.println("Rating: " + rating);
+    }
+}
--- spring-rest-chat/client/src/main/java/cz/profinit/training/springadvanced/springrest/chat/ChatClientApplication.java
+++ spring-rest-chat/client/src/main/java/cz/profinit/training/springadvanced/springrest/chat/ChatClientApplication.java
@@ -1,13 +1,12 @@
 package cz.profinit.training.springadvanced.springrest.chat;
 
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatRating;
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatRatingResponse;
+import cz.profinit.training.springadvanced.springrest.chat.model.ChatUpdate;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.client.RestTemplate;
-
-import cz.profinit.training.springadvanced.springrest.chat.model.ChatRating;
-import cz.profinit.training.springadvanced.springrest.chat.model.ChatRatingResponse;
-import cz.profinit.training.springadvanced.springrest.chat.model.ChatUpdate;
 
 @SpringBootApplication
 public class ChatClientApplication implements CommandLineRunner {
@@ -27,15 +26,15 @@ public class ChatClientApplication implements CommandLineRunner {
         final ChatUpdate start = restTemplate.postForObject(url + "/conversation", null, ChatUpdate.class);
         System.out.println("Start: " + start);
 
-        System.out.println("Send: " + restTemplate.postForObject(url + "/conversation/" + start.getSessionId() + "/message?text=Howdoyoudo", null, ChatUpdate.class));
+        System.out.println("Send: " + restTemplate.postForObject(url + "/conversation/{sessionId}/message?text=Howdoyoudo", null, ChatUpdate.class, start.getSessionId()));
 
         for (int i = 0; i < 3; i++) {
-            System.out.println("Refresh: " + restTemplate.getForObject(url + "/conversation/" + start.getSessionId(), ChatUpdate.class));
+            System.out.println("Refresh: " + restTemplate.getForObject(url + "/conversation/{sessionId}", ChatUpdate.class, start.getSessionId()));
         }
 
         System.out.println("Finish");
-        restTemplate.delete(url + "/conversation/" + start.getSessionId());
+        restTemplate.delete(url + "/conversation/{sessionId}", start.getSessionId());
 
-        System.out.println("Rating: " + restTemplate.postForObject(url + "/conversation/" + start.getSessionId() + "/rating", new ChatRating(10, "scott", "Rather good"), ChatRatingResponse.class));
+        System.out.println("Rating: " + restTemplate.postForObject(url + "/conversation/{sessionId}/rating", new ChatRating(10, "scott", "Rather good"), ChatRatingResponse.class, start.getSessionId()));
     }
 }
