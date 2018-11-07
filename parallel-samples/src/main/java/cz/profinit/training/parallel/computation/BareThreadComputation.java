f7d6b6e0bcf776954ed7fdc05cc37327a2141445
Tomáš Piňos <tomas.pinos@profinit.eu>
2018-11-07 12:51:04 +0100

    Parallel computation samples consolidated.

--- parallel-samples/src/main/java/cz/profinit/training/parallel/computation/BareThreadComputation.java
+++ parallel-samples/src/main/java/cz/profinit/training/parallel/computation/BareThreadComputation.java
@@ -1,26 +1,23 @@
 package cz.profinit.training.parallel.computation;
 
 import cz.profinit.training.parallel.algorithm.ParallelUtils;
+import cz.profinit.training.parallel.algorithm.PrimeCandidate;
 
-import java.util.HashSet;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Set;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class BareThreadComputation {
 
     public static void main(String[] args) throws Exception {
-        Set<Integer> primes = ParallelUtils.withStopWatch(() -> {
-            Set<Integer> set = new HashSet<>();
+        List<PrimeCandidate> primes = ParallelUtils.withStopWatch(() -> {
+            Map<Integer, PrimeCandidate> map = new HashMap<>();
 
             List<Thread> threads = IntStream.range(1, 101).boxed()
                     .map(number ->
-                            new Thread(() -> {
-                                if (ParallelUtils.isPrime(number)) {
-                                    set.add(number);
-                                }
-                            }))
+                            new Thread(() -> map.put(number, new PrimeCandidate(number, ParallelUtils.isPrime(number)))))
                     .collect(Collectors.toList());
 
             threads.forEach(Thread::start);
@@ -33,7 +30,9 @@ public class BareThreadComputation {
                 }
             });
 
-            return set;
+            return map.values().stream()
+                    .filter(PrimeCandidate::isPrime)
+                    .collect(Collectors.toList());
         });
 
         System.out.println("Prime#: " + primes.size());
