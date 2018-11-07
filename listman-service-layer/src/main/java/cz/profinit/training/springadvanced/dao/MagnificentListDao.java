5b183d1648d7e8ffc1ef2607f27bfc5daff31bd8
Tomáš Piňos <tomas.pinos@profinit.eu>
2018-11-07 13:25:36 +0100

    Obsolete modules removed in lab branch.

--- listman-service-layer/src/main/java/cz/profinit/training/springadvanced/dao/MagnificentListDao.java
+++
@@ -1,7 +0,0 @@
-package cz.profinit.training.springadvanced.dao;
-
-import cz.profinit.training.springadvanced.domain.MagnificentList;
-
-public interface MagnificentListDao extends GenericDao<MagnificentList, Integer> {
-
-}
--- spring-integration-helloworld/src/main/java/cz/profinit/training/springadvanced/integration/support/Clock.java
+++
@@ -1,8 +0,0 @@
-package cz.profinit.training.springadvanced.integration.support;
-
-import java.time.LocalDateTime;
-
-public interface Clock {
-
-    LocalDateTime get();
-}
